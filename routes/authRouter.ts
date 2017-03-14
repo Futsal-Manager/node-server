074deb55b52e2994d4b7e38f277a63bf4a915c12
yuhogyun <yoohoogun114@naver.com>
2017-03-14 22:15:31 +0900

    Changed header set

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -15,10 +15,10 @@ export default class AuthRouter {
     }
 
     static success(req, res) {
+        res.status(200).json({msg: 'success login', cookie: req.cookies});
         console.log('==========success headers==========');
         console.log(res.headers);
         console.log('==========success headers==========');
-        res.status(200).json({msg: 'success login', cookie: req.cookies});
     }
 
     static fail(req, res) {
