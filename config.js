ef8954445f257fedabe95b85e2da4428c7950cf6
yuhogyun <yoohoogun114@naver.com>
2017-04-14 15:50:18 +0900

    API Endpoint Changed port

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     MAILGUN_API_KEY: process.env.MAILGUN_API_KEY || 'key-5071c2d41b08df0671c75464deda33f3',
     MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN || 'sandbox9c41f305e6c04852a25aa8d705b3a4c7.mailgun.org',
     PROTOCOL_TOKEN: process.env.PROTOCOL_TOKEN || 'dfisdfn2@#23sdfbjsdfj23klnSDFn1l32nlkndskdskfjs@#f@!#dsf',
-    IMAGE_PROCESSING_API: 'http://ec2-13-124-36-118.ap-northeast-2.compute.amazonaws.com:8000'
+    IMAGE_PROCESSING_API: 'http://ec2-13-124-36-118.ap-northeast-2.compute.amazonaws.com'
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = CONFIG;
